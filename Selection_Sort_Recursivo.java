431cd843826e8a93ff9155ce70fbfd5773bb0c04
brunaletsleticia <140714125+brunaletsleticia@users.noreply.github.com>
2024-07-24 09:22:50 -0300

    Update Selection_Sort_Recursivo.java

--- Selection_Sort_Recursivo.java
+++ Selection_Sort_Recursivo.java
@@ -39,44 +39,4 @@ javac Solucao.java; java Solucao
 [1, 2, 3, 5, 7]
  */
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int[] array = getArrayInteiros(scan);
-		sort(array);
-		scan.close();
-	}
-
-	private static void sort(int[] array) {
-		sort(array, 0);
-	}
-
-	private static void sort(int[] array, int indexAtual) {
-		if (indexAtual < array.length - 1) {
-			int indexOfMin = indexAtual;
-			for (int i = indexAtual + 1; i < array.length; i++) {
-				if (array[i] < array[indexOfMin]) {
-					indexOfMin = i;
-				}
-			}
-			swap(array, indexAtual, indexOfMin);
-			System.out.println(Arrays.toString(array));
-			sort(array, indexAtual + 1);
-		}
-	}
-
-	private static void swap(int[] array, int i, int j) {
-		int aux = array[j];
-		array[j] = array[i];
-		array[i] = aux;
-	}
-
-	private static int[] getArrayInteiros(Scanner scan) {
-		String[] entrada = scan.nextLine().split(" ");
-		int[] array = new int[entrada.length];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = Integer.parseInt(entrada[i]);
-		}
-		return array;
-	}
-
 }
